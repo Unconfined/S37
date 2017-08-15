38e491d6cfdcf5ec1421b87465a20fe0853aa180
Ricky Walker <rickwalk45@gmail.com>
2017-08-14 17:14:26 -0700

    (edit) Remove mongoDB connection until fully functional

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost/s37');
+// mongoose.connect('mongodb://localhost/s37');
 
 app.use(express.static('dist'));
 app.use(morgan('dev'));
